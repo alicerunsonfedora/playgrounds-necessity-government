5716fe538638923181e3f5e274697dcbb55fe636
Marquis Kurt <ubunturox104@gmail.com>
2017-11-09 15:11:10 -1000

    Added playground book template

---
+++ Is Government Necessary?.playgroundbook/Contents/Sources/Accessibility.swift
@@ -0,0 +1,12 @@
+//
+//  Accessibility.swift
+//
+//  Copyright © 2016,2017 Apple Inc. All rights reserved.
+//
+
+import Foundation
+
+public enum Accessibility {
+    /// Determines if the state of portals should be announced as part of voice over announcements.
+    public static var announcePortalStates = false
+}
