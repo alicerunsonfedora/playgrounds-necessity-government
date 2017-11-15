0bf9897e236225cde4550c06b4bad14d30ea37dd
Marquis Kurt <ubunturox104@gmail.com>
2017-11-15 10:48:23 -1000

    Document and edit first page for reference

--- Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Sources/SetUp.swift
+++ Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Sources/SetUp.swift
@@ -5,14 +5,15 @@
 //
 import Foundation
 
-// MARK: Globals
-public let world = GridWorld(columns: 10, rows: 10)
-let actor = Acotr()
+/* Globals
+If you have any objects or items that need to be publicly accessible in the playground page, declare it here. Declaring it in the function playgroundPrologue() will result in a non-local scope error.
+*/
+public let world = GridWorld(columns: 10, rows: 10) //Create a world with the size of 10 x 10.
+let actor = Actor() //Create the actor to be placed in the scene. Any functions requiring the actor to move or do something should be publicized in the Commands.swift file.
 
 public func playgroundPrologue() {
-    Display.coordinateMarkers = true
-
-    world.isCharacterPickerEnabled = true
+    Display.coordinateMarkers = false //Determines if the coordinates should be displayed in the world.
+    world.isCharacterPickerEnabled = true //Determines if the player should be able to choose the avatar. This should ONLY apply in the first world.
     world.successCriteria = .custom(.ignoreGoals, { return false })
     registerAssessment(world, assessment: assessmentPoint)
 
