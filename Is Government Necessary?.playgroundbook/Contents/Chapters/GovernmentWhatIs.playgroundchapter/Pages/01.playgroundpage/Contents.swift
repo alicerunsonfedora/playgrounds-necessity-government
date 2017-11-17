9977a5a96170b28d36db900bd668c10a02806a11
Marquis Kurt <ubunturox104@gmail.com>
2017-11-17 11:40:22 -1000

    Work on first page in chapter 1

--- Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Contents.swift
+++ Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Contents.swift
@@ -1,25 +1,27 @@
 //#-hidden-code
-//
-//  Contents.swift
-//
-//  Copyright © 2016,2017 Apple Inc. All rights reserved.
-//
-//#-end-hidden-code
-/*:#localized(key: "FirstProseBlock")
-Use this playground to create any puzzle world you want!
+import UIKit
+import PlaygroundSupport
 
+PlaygroundPage.current.needsIndefiniteExecution = true //Because concurrent code is used, we must allow Swift to make the playground run indefinitely until the end.
+
+let asyncQueue = DispatchQueue(label: "asyncQueue", attributes: .concurrent) //Start the concurrent code process
+
+asyncQueue.async {
+    nicolle.move(distance: 4)
+    nicolle.turnLeft()
+} //As nicolle moves and turns left...
+supervisor.move(distance: 4)
+supervisor.turnRight() //The supervisor will move and turn right...
+//#-end-hidden-code
+
+//Write your information here in markup.
+/*:
+# What is a government?
 */
 //#-hidden-code
 playgroundPrologue() //Builds the world
 //#-end-hidden-code
-//#-code-completion(everything, hide)
-//#-code-completion(literal, show, color, array)
-//#-code-completion(currentmodule, show)
-//#-code-completion(description, show, "[Int]")
-//#-code-completion(identifier, show, isOnOpenSwitch, if, func, for, while, moveForward(), turnLeft(), turnRight(), collectGem(), toggleSwitch(), isBlocked, north, south, east, west, Water, Expert, Character, (, ), (), turnLockUp(), turnLockDown(), isOnClosedSwitch, var, let, ., =, <, >, ==, !=, +=, +, -, isBlocked, move(distance:), Character, Expert, (, ), (), Portal, color:, (color:), Block, Gem, Stair, Switch, Platform, (onLevel:controlledBy:), onLevel:controlledBy:, PlatformLock, jump(), true, false, turnLock(up:numberOfTimes:), world, place(_:facing:at:), place(_:between:and:), removeBlock(atColumn:row:), isBlockedLeft, &&, ||, !, isBlockedRight, Coordinate, column:row:), (column:row:), column:row:, place(_:at:), remove(at:), insert(_:at:), removeItems(at:), append(_:), count, column(_:), row(_:), removeFirst(), removeLast(), randomInt(from:to:), removeAll(), allPossibleCoordinates, danceLikeNoOneIsWatching(), turnUp(), breakItDown(), grumbleGrumble(), argh(), coordinates(inRows:), coordinates(inColumns:intersectingRows:), name:, (name:), byte, blu, hopper, randomBool(), height(at:), movePlatforms(up:numberOfTimes:), height(at:), coordinates(inColumns:), existingGems(at:), existingSwitches(at:), existingCharacters(at:), existingExperts(at:), existingBlocks(at:), existingWater(at:), placeBlocks(at:), placeWater(at:), placeGems(at:), CharacterName, numberOfBlocks(at:), column, row)
-//#-editable-code Tap to enter code
 
-//#-end-editable-code
 //#-hidden-code
 playgroundEpilogue()
 //#-end-hidden-code
--- Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Sources/SetUp.swift
+++ Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Sources/SetUp.swift
@@ -3,13 +3,26 @@
 //
 //  Copyright © 2016,2017 Apple Inc. All rights reserved.
 //
+import UIKit
 import Foundation
+import PlaygroundSupport
+
+typealias Character = Actor //Whenever someone wants to create a character, it can be done using "Character" instead of "Actor"
 
 /* Globals
 If you have any objects or items that need to be publicly accessible in the playground page, declare it here. Declaring it in the function playgroundPrologue() will result in a non-local scope error.
 */
 public let world = GridWorld(columns: 10, rows: 10) //Create a world with the size of 10 x 10.
-let actor = Actor() //Create the actor to be placed in the scene. Any functions requiring the actor to move or do something should be publicized in the Commands.swift file.
+let actor = Actor() //Create the actor to be placed in the scene. Any functions requiring the actor to move or do something should be publicized in the Commands.swift file. This page does NOT require the actor to be used, so this line should be discarded.
+let civilian = Character(name: .byte)
+let secondCivilian = Character(name: .byte)
+let thirdCivilian = Character(name: .byte)
+let fourthCivilian = Character(name: .byte)
+let fifthCivilian = Character(name: .byte)
+
+//Only nicolle and supervisor are made public so the playground world can freely access it.
+public let nicolle = Character(name: .blu)
+public let supervisor = Character(name: .hopper)
 
 public func playgroundPrologue() {
     Display.coordinateMarkers = false //Determines if the coordinates should be displayed in the world.
@@ -18,19 +31,54 @@ public func playgroundPrologue() {
     registerAssessment(world, assessment: assessmentPoint)
 
     //Create your array literals for coordinates here.
+    let wholeSide = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     //Place your world generation here.
+    world.placeBlocks(at: [Coordinate(column: 5, row: 1)])
+
+    for _ in 1 ... 2 {
+        world.placeBlocks(at: [Coordinate(column: 4, row: 1)])
+    }
+
+    world.placeBlocks(at: [Coordinate(column: 3, row: 1)])
+
+    world.place(Stair(), facing: east, at: Coordinate(column: 6, row: 1))
+    world.place(Stair(), facing: west, at: Coordinate(column: 2, row: 1))
+    world.place(Stair(), facing: west, at: Coordinate(column: 3, row: 1))
 
     //Place gem and switch insertions here.
 
-    //Finally, place the actor in the scene here.
-    world.place(actor, facing: north, at: Coordinate(column: 1, row: 1))
+    /* Finally, place the actor in the scene here. There are many characters in this scene, so each of them will be placed in accordingly. Usually, you'll initialize the actor here as so:
+
+    world.place(actor, at: Coordinate(column: 1, row: 1))
+    */
+    world.place(civilian, at: Coordinate(column: 2, row: 5))
+    world.place(secondCivilian, at: Coordinate(column: 3, row: 3))
+    world.place(thirdCivilian, at: Coordinate(column: 5, row: 5))
+    world.place(fourthCivilian, at: Coordinate(column: 2, row: 8))
+    world.place(fifthCivilian, at: Coordinate(column: 7, row: 7))
+    world.place(nicolle, facing: east, at: Coordinate(column: 0, row: 1))
+    world.place(supervisor, facing: west, at: Coordinate(column: 9, row: 1 ))
 
     //// ----
     // Any items added or removed after this call will be animated.
     finalizeWorldBuilding(for: world)
     //// ----
 }
+
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let asyncQueue = DispatchQueue(label: "asyncQueue", attributes: .concurrent)
+
+asyncQueue.async {
+    nicolle.move(distance: 4)
+    nicolle.turnLeft()
+}
+supervisor.move(distance: 4)
+supervisor.turnRight()
 
 public func presentWorld() {
     setUpLiveViewWith(world)
