5e5dedb55ac1e669311401c3cc0b9667a74f0402
Marquis Kurt <ubunturox104@vivaldi.net>
2017-11-21 08:42:48 -1000

    Added second page
    Included the second page about government origins.

--- Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Contents.swift
+++ Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/01.playgroundpage/Contents.swift
@@ -7,16 +7,14 @@ import PlaygroundSupport
 - Callout(Definition):
 A **government** is defined as an institution that carries out public policies that attempt to reflect the will of its people at the discretion of those in power.
 
-Take a look at the government on the right. This government consists of a supervisor, the ruler, and the citizens. The blue character, Ruler 'N', runs the government and the supervisor (green character) assists in writing laws.
+Take a look at the government on the right. This government consists of a supervisor, the ruler, and the citizens (orange characters). The blue character, Ruler 'N', runs the government and the supervisor (green character) assists in writing laws.
 
-While there is no code for you to run, you can hit the **Run My Code** button to see the leaders proceed in. In the next couple of pages, you'll determine what type of government is best suited for these people.
+While there is no code for you to type in for execution, you can hit the **Run My Code** button to see the leaders proceed in. In the next couple of pages, you'll determine what type of government is best suited for these people.
 */
 //#-hidden-code
 playgroundPrologue() //Builds the world
 let rulerN = nicolle
 //#-end-hidden-code
-
-//#-editable-code
 PlaygroundPage.current.needsIndefiniteExecution = true //Because concurrent code is used, we must allow Swift to make the playground run indefinitely until the end.
 
 let asyncQueue = DispatchQueue(label: "asyncQueue", attributes: .concurrent) //Start the concurrent code process
@@ -24,10 +22,10 @@ let asyncQueue = DispatchQueue(label: "asyncQueue", attributes: .concurrent) //S
 asyncQueue.async {
     rulerN.move(distance: 4)
     rulerN.turnLeft()
-} //As nicolle moves and turns left...
+} //As Ruler N moves and turns left...
 supervisor.move(distance: 4)
 supervisor.turnRight() //The supervisor will move and turn right...
-//Write your information here in marku
+//#-editable-code
 //#-end-editable-code
 
 //#-hidden-code
---
+++ Is Government Necessary?.playgroundbook/Contents/Chapters/GovernmentWhatIs.playgroundchapter/Pages/02.playgroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//#-hidden-code
+import UIKit
+import PlaygroundSupport
+//#-end-hidden-code
+/*:
+# What are the origins of government?
+It's not clearly known as to how governments came to be. However, there are four theories that try to explain how governments were formed:
+
+* The _`force` theory_ imposes ideals of someone or a group of people by means of physical, economical, or psychological behaviors.
+* The _`evolutionary` theory_ works by creating a group or a clan in which they elect a leader to lead them.
+* The _`divine` right theory_ assumes that a leader is "sent from above" or is related to religious figures and therefore should be superior to others, allowing them to be the leader and create a cult around them.
+* The _social `contract` theory_ is what the United States has in which the people agree to make a government and create a contract for it.
+
+Using the code below, try to figure out which origin best matches this government.
+*/
+//#-hidden-code
+playgroundPrologue()
+//#-end-hidden-code
+//#-code-completion(identifier, show, divine, evolutionary, force, contract)
+
+useGovernmentTheory(/*#-editable-code Tap to enter type*//*#-end-editable-code*/)
+checkIfGovernmentOriginMatches()
+
+//#-hidden-code
+playgroundEpilogue()
+//#-end-hidden-code
