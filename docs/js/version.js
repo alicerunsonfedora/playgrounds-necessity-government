442472b9179b7a4530d84eb41e60fb454c7f523d
Marquis Kurt <ubunturox104@gmail.com>
2017-11-28 07:44:37 -1000

    Update site details
    Included a carousel of screenshots, removed variable object 'version' in place of JSON file, loaded all external data in version.js

--- docs/js/version.js
+++ docs/js/version.js
@@ -1,4 +1,21 @@
-var version = {
-  number: "1.0.0b1",
-  stage: "orange" //Green = stable, orange = pre-release
-};
+$(document).ready(function(){
+  $('.carousel').carousel();
+  $('.modal').modal();
+
+  $.getJSON("version.json",{}, function( data ){
+      document.getElementById('version-id').innerHTML = data.build;
+
+      var stage = data.stage;
+
+      if (stage == "stable") {
+        $(document.getElementById('download-button')).addClass("green");
+      } else if (stage == "dev") {
+        $(document.getElementById('download-button')).addClass("orange");
+      } else {
+        $(document.getElementById('download-button')).addClass("brown");
+      }
+
+      document.getElementById('version-report').innerHTML = "The current version available is <b>" + data.build + "</b> under the <b>" + data.stage + "</b> channel, released on: <b>" + data.buildDate + "</b>.";
+  });
+
+});
